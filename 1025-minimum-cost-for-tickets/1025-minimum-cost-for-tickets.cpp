fd08f551e20053dda8fbd6c4e7512dbcebf3098b
GagansharmaGit <142130700+GagansharmaGit@users.noreply.github.com>
2023-09-13 00:24:50 +0530

    Time: 6 ms (31.98%), Space: 9.9 MB (19.58%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int solve(int i,vector<int>&days,vector<int>&costs,vector<int>&dp){
-        if(i >= days.size()) return 0;
-        if(dp[i] != -1) return dp[i];
-        //for cost[0]
-        int cost_2 = costs[0] + solve(i+1,days,costs,dp);
-        //for 7 day cost
-        int passenger_day_7 = days[i]+7-1;
-        int j=i;
-        while(j<days.size() and days[j] <= passenger_day_7){
-            j++;
-        }
-        int cost_7 = costs[1] + solve(j,days,costs,dp);
-        //for_30 days ticket price
-        int passenger_day_30 = days[i] +30-1;
-        int k=i;
-        while(k<days.size() and days[k] <= passenger_day_30){
-            k++;
-        }
-        int cost_30 = costs[2] + solve(k,days,costs,dp);
-
-        return dp[i] = min(cost_2,min(cost_7,cost_30));
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size()+1,-1);
-        return solve(0,days,costs,dp);
+        unordered_set<int>st;
+        for(int i=0;i<days.size();i++){
+            st.insert(days[i]);
+        }
+        vector<int>dp(days.back()+1,0);
+        dp[0] = 0;
+        int last_day = days.back();
+        for(int i = 1;i<=last_day;i++){
+            if(st.find(i) == st.end()){
+                dp[i] = dp[i-1]; //skipping the day which we do not have to travel so we just copt the result of prev state
+                continue;
+            }
+            int pass_1_cost = costs[0] + dp[max(i-1,0)];
+            int pass_7_cost = costs[1] + dp[max(i-7,0)];
+            int pass_30_cost = costs[2] + dp[max(i-30,0)];
+            dp[i] = min(pass_1_cost,min(pass_7_cost,pass_30_cost));
+        }
+        return dp[last_day];
     }
 };
