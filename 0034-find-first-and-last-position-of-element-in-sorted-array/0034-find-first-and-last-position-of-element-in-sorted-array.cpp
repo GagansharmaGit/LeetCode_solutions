69d5eb27911de0212118c83daab6cd4ea27dae8d
Gagan Sharma <sharmagagan192@gmail.com>
2024-08-03 19:43:04 +0530

    Time: 10 ms (18.01%), Space: 16.3 MB (31.9%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
-    int find_first_position(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        int result = -1;
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            if(nums[mid] == target) {
-                result = mid; //possibly my answer
-                r = mid-1;     //but lets look at left more
-            } else if(nums[mid] > target) {
-                r = mid-1;
-            } else {
-                l = mid+1;
+    int firstOcc(vector<int>&nums,int &target,int &size){
+        int start = 0;
+        int end = size -1;
+        int ind = -1;
+        while(start <= end){
+            int mid = (start+end) /2;
+
+            if(nums[mid] == target){
+                ind = mid;
+                start = mid + 1;
+            }else if(nums[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
-        
-        return result;
+        return ind;
     }
-    
-    int find_last_position(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        int result = -1;
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            if(nums[mid] == target) {
-                result = mid; //possibly my answer
-                l = mid+1;   //but lets look at right more
-            } else if(nums[mid] > target) {
-                r = mid-1;
-            } else {
-                l = mid+1;
+
+
+    int secOcc(vector<int>&nums,int &target,int &size){
+        int start = 0;
+        int end = size -1;
+        int ind = -1;
+        while(start <= end){
+            int mid = (start + end) /2;
+            if(nums[mid] == target){
+                ind = mid;
+                end = mid -1;
+            }else if(nums[mid] > target){
+                end = mid -1;
+            }else{
+                start = mid +1;
             }
         }
-        
-        return result;
+        return ind;
     }
+
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = find_first_position(nums, target);
-        int r = find_last_position(nums, target);
-        
-        return {l, r};
+        int size = nums.size();
+        int first = firstOcc(nums,target,size);
+        int second = secOcc(nums,target,size);
+
+        return {second,first};
+        // return {first,second};
     }
 };
-
